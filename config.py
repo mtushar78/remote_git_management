6c652384889cbeaabb14d937dd4c20d8546ab1ba
Tushar <mtushar78@gmail.com>
2021-04-05 21:41:44 +0600

    first commit

---
+++ config.py
@@ -0,0 +1,13 @@
+configs = {
+    '172.31.2.3':{'creds':['172.31.2.3',8822,'root','Qpay@Nbl2018'],
+                  'commands':{'pull': 'cd / && ls'}},
+    '172.31.2.36':['172.31.2.36',8822,'root','quickpay2@dc'],
+    '172.31.2.35':['172.31.2.35',22,'root','cDRtGV@2021'],
+    '172.104.185.128':{'creds':['172.104.185.128', 22, 'root', 'diit@321#'],
+                       'commands':{
+                           'pull':['cd /var/ && ls'],
+                           'push': ['cd /var/ && python3 --version']
+                           }
+                       }
+    }
+
